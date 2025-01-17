d7119911c69b3a1b7db4e59a1de52995b2440677
Saikrishna1504 <saikrishna26918@gmail.com>
2025-01-17 07:03:38 +0000

    duchamp: Import MiuiCamera
    Change-Id: Ie8c3f8cfa1a962a1071af543ffafac986a3bd339
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,7 @@ TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
@@ -153,6 +154,9 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+
+# MiuiCamera
+CAMERA_PACKAGE_NAME := com.android.camera
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
