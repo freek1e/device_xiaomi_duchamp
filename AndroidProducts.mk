bd0d85c782d51fb944e0018dc03754e095cd9213
Kemji <kurtandrieasoy17@gmail.com>
2025-10-06 07:45:15 +0000

    Revert "duchamp: Infinity Bringup"
    This reverts commit 34e98fa28f8961ee2822491681816efc9bf9f3ae.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_duchamp.mk
+    $(LOCAL_DIR)/lineage_duchamp.mk
--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -11,14 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/duchamp/device.mk)
 
-# Inherit some common InfinityX stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
-
-# Infinity X Stuff
-INFINITY_BUILD_TYPE := UNOFFICIAL
-INFINITY_MAINTAINER := "Kemji"
-TARGET_HAS_UDFPS := true
-WITH_GAPPS := true
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_duchamp
 PRODUCT_DEVICE := duchamp
