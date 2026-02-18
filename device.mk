ef6e1a74abd60b1367ad27f08e01db10f77d0c92
rio004 <riyon336@gmail.com>
2026-02-18 14:15:34 +0000

    duchamp: Switch to prebuilt bluetooth audio implemenatiation
    * Disable LE offloading for now
    
    Change-Id: I9e2b814c5ac40f9b352e6e37fbc60682938ad365
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
     audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
@@ -82,15 +81,13 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth-service.mediatek \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth-service.mediatek
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1220
