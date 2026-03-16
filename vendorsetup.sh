995cbbbf243802519e810d6522f94472b0de7ab1
MT6897A <tomilindanya@mail.ru>
2026-03-16 18:44:30 +0000

    duchamp: Add vendorsetup.sh and some hardware patches
    Some patches (presented in the commit) cannot be generalized to all devices.
    
    This commit adds duchamp specific patches for maximum compatibility.

---
+++ vendorsetup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Applying some patches..."
+
+DEVICE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PATCH_DIR="$DEVICE_DIR/patches"
+ROOT_DIR="$DEVICE_DIR/../../.."
+
+apply_patch() {
+    local target_dir="$ROOT_DIR/$1"
+    local patch_file="$PATCH_DIR/$2"
+
+    if [ ! -d "$target_dir" ]; then
+        echo " Target directory $target_dir does not exist. Skipping."
+        return
+    fi
+
+    echo " Checking $2 for $1..."
+    
+    pushd "$target_dir" > /dev/null || return
+
+    if git apply --check --reverse --recount "$patch_file" > /dev/null 2>&1; then
+        echo "    Patch is already applied. Skipping."
+    else
+        git apply --recount --whitespace=fix "$patch_file" > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "    Successfully applied."
+        else
+            echo "    Failed to apply! Reason:"
+            git apply --check --recount "$patch_file"
+        fi
+    fi
+
+    popd > /dev/null
+}
+
+apply_patch "hardware/xiaomi" "0001-TouchFeature.patch"
+apply_patch "hardware/mediatek" "0002-Power.patch"
+apply_patch "hardware/xiaomi" "0003-DisplayFeature.patch"
+apply_patch "hardware/xiaomi" "0004-UDFPS.patch"
+apply_patch "hardware/xiaomi" "0005-FP-Base.patch"
+
+
+echo "Done"
