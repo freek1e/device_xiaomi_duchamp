faed32391a437be333c18755d5bf37d7be1b9977
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-04-01 16:38:23 +0530

    duchamp: dolby: Sync LunarisDolby and update configs
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- dolby/dolby.mk
+++ dolby/dolby.mk
@@ -31,7 +31,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DOLBY_PATH)/vendor.dolby.hardware.dms@2.0-service.xml
 DEVICE_MANIFEST_FILE += $(DOLBY_PATH)/vendor.dolby.media.c2.xml
-    
+
 # Configs
 PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/configs/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
 # Overlays    
 PRODUCT_PACKAGES += \
     DolbyFrameworksResCommon
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(DOLBY_PATH)/configs/permissions/privapp-permissions-dolby.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-dolby.xml
 
 # Spatial Audio
 PRODUCT_COPY_FILES += \
