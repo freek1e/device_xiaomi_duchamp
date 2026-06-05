05b40235e95604f798580ab810498cd3706e60d6
freek1e <akrpakin34@gmail.com>
2026-06-05 19:14:14 +0300

    duchamp: improvements remove unneded fix.patch & add miui camera to boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,3 +205,5 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
 include vendor/xiaomi/duchamp/BoardConfigVendor.mk
+
+include device/xiaomi/duchamp-miuicamera/BoardConfig.mk
