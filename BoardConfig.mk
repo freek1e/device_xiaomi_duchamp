832b927664a0149e75f5cbe3e3e4caa3121a5d38
freek1e <akrpakin34@gmail.com>
2026-06-08 23:34:12 +0300

    duchamp: Remove miui camera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,4 +206,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # Inherit the proprietary files
 include vendor/xiaomi/duchamp/BoardConfigVendor.mk
 
-include device/xiaomi/duchamp-miuicamera/BoardConfig.mk
